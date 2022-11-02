acf0660f0c03bc7c9d46077ff01404f250dd5a52
unknown <kumarrohitsingh824@gmail.com>
2022-11-02 18:04:33 +0530

    code fixed m1

--- prime.js
+++ prime.js
@@ -1,15 +1,15 @@
-function prime(num){
-let count=1;
-for(i=0;i<=num;i++){
+function prime(number){
+let counter=0;
+for(i=1;i<=number;i++){
 if(num%i==0){
-count++;}
+counter++;}
 }
-if(count==1){
+if(counter==2){
 return true;}
 else {return false}
 }
 
-let ans= prime(39)
+let ans= prime(13)
 if(ans==true){
 console.log("prime")}
 else{console.log("not prime")}
